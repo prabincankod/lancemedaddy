8570adf695cb871fc073144a4b75451e623bb10b
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-02-09 14:14:14 +0000

    :rocket: added update todo

--- controllers/todo.controller.ts
+++ controllers/todo.controller.ts
@@ -49,4 +49,36 @@ const deleteTodo = async (req: Request, res: Response) => {
   }
 };
 
-export { createTodo, deleteTodo };
+const updateTodo = async (req: Request, res: Response) => {
+  try {
+    const { name, status } = req.body;
+    const todo = await prismaClient.todo.findUnique({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+
+    if (
+      status !== ("DONE" || "INIT" || "PROGRESS" || "TEST") &&
+      status !== undefined
+    ) {
+      return res.send({ message: `${status} is not a valid status` });
+    }
+    // @ts-ignore
+    if (todo?.userId === req?.id) {
+      const updet = await prismaClient.todo.update({
+        data: { name, status },
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      res.send(updet);
+    } else {
+      return res.status(402).send({ message: "not your piece of cake" });
+    }
+  } catch (error) {
+    res.status(401).send(error);
+  }
+};
+
+export { createTodo, deleteTodo, updateTodo };
--- routes/todo.route.ts
+++ routes/todo.route.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { createTodo, deleteTodo } from "../controllers/todo.controller";
+import {
+  createTodo,
+  deleteTodo,
+  updateTodo,
+} from "../controllers/todo.controller";
 import auth from "../middlewares/auth";
 
 const todoRouter = Router();
 
 todoRouter.post("/add", auth, createTodo);
 todoRouter.delete("/delete/:id", auth, deleteTodo);
+todoRouter.patch("/update/:id", auth, updateTodo);
 
 export default todoRouter;
